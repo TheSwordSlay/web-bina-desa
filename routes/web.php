e60b31d093f5ff4915805246ac89f067973ac9b0
AgiAgustianDavi <agidavi6@gmail.com>
2023-10-27 20:15:51 +0700

    menambhakan hal agi

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/halaman_agi', function () {
+    return view('hal_agi');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');

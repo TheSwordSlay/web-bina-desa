c3a499e6e14bb1e3ede90498978486b783375e2d
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2023-10-30 20:21:51 +0700

    navbar dimasukin ke file lain

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,36 +12,7 @@
 
 <body>
     <!-- navbar  -->
-    <nav class="navbar navbar-expand-lg bg-primary-subtle">
-        <div class="container-fluid">
-            <a class="navbar-brand p-0 m-0 fs-1 fw-bold" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                Logo
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto ps-3 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">page 2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Page 3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Page 4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Page 5</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @include('navbar')
     <!-- navbar end  -->
 
     <!-- home  -->

c1b67fe46ad0ef745d94f1921cd755591a9d6c1e
AgiAgustianDavi <agidavi6@gmail.com>
2023-10-31 21:42:35 +0700

    update file home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,55 +18,20 @@
     @include('navbar')
     <!-- navbar end  -->
 
-    <!-- slide show  -->
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="img/slideshow.jpg" width="100" height="500" class="d-block w-100 img-fluid" alt="foto">
-                <div class="carousel-caption">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/slideshow.jpg" width="100" height="500" class="d-block w-100 img-fluid" alt="foto">
-                <div class="carousel-caption">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/slideshow.jpg" width="100" height="500" class="d-block w-100 img-fluid" alt="foto">
-                <div class="carousel-caption">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/slideshow.jpg" width="100" height="auto" class="d-block w-100 img-fluid" alt="foto">
-                <div class="carousel-caption">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="img/slideshow.jpg" width="100" height="500" class="d-block w-100 img-fluid" alt="foto">
-                <div class="carousel-caption">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
+    <!-- home -->
+    <section class="home">
+        <div class="container-fluid d-flex justify-content-center align-items-center ">
+            <div class="content text-center">
+                <h1 class="text-light">Selamat Datang di Desa ...</h1>
+                <a class="btn btn-primary" href="#" role="button">Selengkapnya</a>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-    <!-- slidshow end -->
+    </section>
+    <!-- home end -->
+
+    <!-- maps desa -->
+    <!-- maps desa end  -->
+    
 
     <!-- news -->
     <div class="container text-center">
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg bg-primary-subtle shadow">
-        <div class="container-fluid">
-            <a class="navbar-brand p-0 m-0 fs-1 fw-bold" href="#">
+<nav class="navbar navbar-expand-lg navbar-dark shadow-lg bg-primary fixed-top">
+        <div class="container">
+            <a class="navbar-brand p-0 m-0 fs-3 fw-medium" href="#">
                 <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
                 Logo
             </a>
@@ -8,20 +8,20 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto ps-3 pe-3">
-                    <li class="nav-item">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item ps-3 pe-3">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ps-3 pe-3">
                         <a class="nav-link" href="#">page 2</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ps-3 pe-3">
                         <a class="nav-link" href="#">Page 3</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ps-3 pe-3">
                         <a class="nav-link" href="#">Page 4</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ps-3 pe-3">
                         <a class="nav-link" href="#">Page 5</a>
                     </li>
                 </ul>

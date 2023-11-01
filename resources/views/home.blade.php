3674d7749e3b74738855eb221abe32f5493b4a40
AgiAgustianDavi <agidavi6@gmail.com>
2023-11-01 14:43:59 +0700

    update file home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,8 +31,8 @@
     <!-- home end -->
 
     <!-- maps & perangkat desa -->
-    <section class="map pt-4 mb-4 bg-primary-subtle">
-        <div class="container text-center pb-4">
+    <section class="map pt-4 mb-5">
+        <div class="container text-center pb-4 mb-5">
             <div class="pb-2">
                 <h1 class="pb-2">Peta Desa</h1>
             </div>
@@ -43,124 +43,27 @@
     </section>
     <!-- maps desa end  -->
 
+    <!-- penjelasan desa  -->
+    <section class="penjelasan  bg-primary-subtle">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores aliquid enim, culpa quod quibusdam ipsum iure autem, doloribus debitis quidem quasi odit tenetur deserunt odio sed. Quia quis error unde cumque officiis temporibus illo molestiae recusandae impedit laudantium vero deleniti eum, doloremque dignissimos voluptates esse, cum hic modi nam quidem. A atque reprehenderit maxime magnam distinctio cumque minus necessitatibus autem magni nam, suscipit ratione perspiciatis praesentium. Dicta, voluptates accusantium optio error dolor voluptatem minima officiis totam suscipit officia aut eligendi repellat enim debitis praesentium iure corrupti nostrum quas labore fugiat cumque quidem veritatis. Culpa exercitationem delectus reiciendis. Dolore similique officiis pariatur temporibus minima? Qui quos quaerat fuga minima animi, labore quam ab distinctio, soluta eos ipsam! Quis recusandae, accusantium at nesciunt amet, laborum et iusto soluta in laudantium ex praesentium cum? Consectetur, inventore accusantium autem asperiores consequuntur corrupti qui laboriosam nostrum cum ratione, magni harum nihil eligendi soluta tempore cupiditate in ducimus distinctio debitis. Voluptates dicta modi nemo, facilis tempore blanditiis ex ipsum cumque sapiente illo, ipsa, fuga quos enim repudiandae eos eligendi dolorem impedit consequatur asperiores voluptatem obcaecati. Fugiat distinctio sed, veritatis mollitia ducimus natus laborum quibusdam nisi inventore error quidem tempore laudantium, pariatur neque aperiam cumque ipsa repellendus.
+                </div>
+                <div class="paralax col-md-6 mt-5 mb-5 rounded-start-5"></div>
+            </div>
+        </div>
+    </section>
+    <!-- penjelasan desa end  -->
+
 
     <!-- news -->
     <div class="container text-center">
-        <div class="row p-5">
-            <div class="col-md-8 order-md-1 order-2">
-                <h1 class="fw-bold fs-1">Berita Terkini</h1>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="mt-5 pb-4">
+            <h1>Berita Terkini</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
@@ -176,8 +79,117 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 order-md-2 order-1 pt-4 pb-4">
-                2 of 2
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="img/berita.jpeg" class="img-fluid rounded-start" alt="foto">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
